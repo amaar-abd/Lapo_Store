15a6cee54c78a2629f6f1413d224bdcba659edd2
Amaar Abd <abdamaar64@gmail.com>
2026-04-15 11:49:48 +0300

    config: add Supasbase credentials and initalize in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:lapo_app/core/constants/api_constants.dart';
 import 'package:lapo_app/core/routes/app_routes.dart';
 import 'package:lapo_app/core/routes/route_generator.dart';
 import 'package:lapo_app/core/theme/app_theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: ApiConstants.supabaseUrl,
+    anonKey: ApiConstants.supabaseAnonKey,
+  );
   runApp(const LapoApp());
 }
 
 class LapoApp extends StatelessWidget {
   const LapoApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +25,6 @@ class LapoApp extends StatelessWidget {
       initialRoute: AppRoutes.splashView,
       theme: AppTheme.mainTheme,
       debugShowCheckedModeBanner: false,
-      );
+    );
   }
 }
