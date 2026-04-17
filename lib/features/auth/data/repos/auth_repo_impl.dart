5fe42203ab006946ced4bbb760aaf944a2d522f9
Amaar Abd <abdamaar64@gmail.com>
2026-04-17 19:14:15 +0300

    feat(auth): implement signIn in AuthRepository

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:lapo_app/core/errors/custom_exception.dart';
+import 'package:lapo_app/core/errors/supabase_error.dart';
 import 'package:lapo_app/core/errors/failures.dart';
 import 'package:lapo_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:lapo_app/features/auth/domain/entities/user_entity.dart';
@@ -24,7 +25,30 @@ class AuthRepoImpl implements AuthRepo {
 
       return right(userEntity);
     } on ServerException catch (e) {
-      return left(ServerFailure(message: e.message));
+      return left(
+        ServerFailure(message: SupabaseError.mapSupabaseError(e.message)),
+      );
+    } catch (e) {
+      return left(ServerFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userEntity = await authRemoteDataSource.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      return right(userEntity);
+    } on ServerException catch (e) {
+      return left(
+        ServerFailure(message: SupabaseError.mapSupabaseError(e.message)),
+      );
     } catch (e) {
       return left(ServerFailure(message: e.toString()));
     }
--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -8,4 +8,9 @@ abstract class AuthRepo {
     required String email,
     required String password,
   });
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
 }
