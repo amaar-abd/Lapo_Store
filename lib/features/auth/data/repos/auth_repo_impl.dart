b9314095adeebeed55cfb019ae6af3da0edd46d1
Amaar Abd <abdamaar64@gmail.com>
2026-04-17 14:55:08 +0300

    feat(auth): implement  AuthRepository

--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,5 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:lapo_app/core/errors/custom_exception.dart';
+import 'package:lapo_app/core/errors/failures.dart';
+import 'package:lapo_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:lapo_app/features/auth/domain/entities/user_entity.dart';
 import 'package:lapo_app/features/auth/domain/repo/auth_repo.dart';
 
-class AuthRepoImpl implements AuthRepo{
-  
+class AuthRepoImpl implements AuthRepo {
+  final AuthRemoteDataSource authRemoteDataSource;
+
+  AuthRepoImpl({required this.authRemoteDataSource});
+  @override
+  Future<Either<Failure, UserEntity>> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userEntity = await authRemoteDataSource.signUpWithEmailAndPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+
+      return right(userEntity);
+    } on ServerException catch (e) {
+      return left(ServerFailure(message: e.message));
+    } catch (e) {
+      return left(ServerFailure(message: e.toString()));
+    }
+  }
 }
--- lib/features/auth/domain/repo/auth_repo.dart
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -1,1 +1,11 @@
-abstract class AuthRepo {}
+import 'package:dartz/dartz.dart';
+import 'package:lapo_app/core/errors/failures.dart';
+import 'package:lapo_app/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> signUpWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+}
